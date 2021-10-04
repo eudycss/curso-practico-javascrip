262997cd6ebbd880800fab59ca97a2a02755657b
eudycss <euddyk@gmail.com>
2021-10-03 22:17:38 -0500

    Challenge arrays y switch

--- descuentos.js
+++ descuentos.js
@@ -1,20 +1,39 @@
 // const precioOriginal=120;
 // const descuento=18;
 
-function calcularPrecioConDescuento(precio,descuento){
-
-    const porcentajePrecioConDescuento=100-descuento;
-    const precioConDescuento=(precio*porcentajePrecioConDescuento)/100
-    return precioConDescuento
-
+function calcularPrecioConDescuento(precio, descuento) {
+  const porcentajePrecioConDescuento = 100 - descuento;
+  const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+  return precioConDescuento;
 }
-function onClickButtonPriceDiscount(){
-const inputPrice = document.getElementById("InputPrice")
-const priceValue=inputPrice.value
-const inputDiscount = document.getElementById("InputDiscount")
-const discountValue=inputDiscount.value
+function onClickButtonPriceDiscount() {
+  const inputPrice = document.getElementById("InputPrice");
+  const priceValue = inputPrice.value;
+  // const inputDiscount = document.getElementById("InputDiscount")
+  // const discountValue=inputDiscount.value
+  const inputCoupon = document.getElementById("InputCoupon");
+  const couponValue = inputCoupon.value;
 
-const precioConDescuento= calcularPrecioConDescuento(priceValue, discountValue)
-const resultP1=document.getElementById("resultP")
-resultP1.innerText="El precio con descuento son: $"+precioConDescuento;
+  let descuento;
+
+  switch (couponValue) {
+    case  coupons[0]: // "JuanDC_es_Batman"
+      descuento = 15;
+      break;
+    case coupons[1]: // "pero_no_le_digas_a_nadie"
+      descuento = 30;
+      break;
+    case coupons[2]: // "es_un_secreto"
+      descuento = 25;
+      break;
+  }
+
+  const precioConDescuento = calcularPrecioConDescuento(
+    priceValue,
+    descuento
+  );
+  const resultP1 = document.getElementById("resultP");
+  resultP1.innerText = "El precio con descuento son: $" + precioConDescuento;
 }
+
+const coupons = ["Cupon 1", "Cupon 2", "Cupon 3"];
